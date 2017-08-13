aa1046c4173c3c8310c56549c3284ae2c10e73be
xaanit <shadowjacob@yahoo.com>
2017-08-13 16:04:26 -0500

    Standard library start

---
+++ src/main/java/me/xaanit/life/std/lib/ParserEquality.java
@@ -0,0 +1,239 @@
+package me.xaanit.life.std.lib;
+
+import me.xaanit.life.annotations.LifeExecutable;
+
+@SuppressWarnings("unused")
+public class ParserEquality {
+
+    private ParserEquality() {
+        // Not to be used.
+    }
+
+
+    @LifeExecutable
+    public static boolean equals(int arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean equals(float arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean equals(double arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean equals(long arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean equals(char arg1, char arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean equals(String arg1, String arg2, boolean ignoreCase) {
+        return ignoreCase ? arg1.equalsIgnoreCase(arg2) : arg1.equals(arg2);
+    }
+
+    @LifeExecutable
+    public static boolean lessThan(int arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(float arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(double arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(long arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(int arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(float arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(double arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(long arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(int arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(float arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(double arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(long arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(int arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(float arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(double arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean lessThan(long arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+    @LifeExecutable
+    public static boolean greaterThan(int arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(float arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(double arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(long arg1, int arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(int arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(float arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(double arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(long arg1, float arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(int arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(float arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(double arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(long arg1, double arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(int arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(float arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(double arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+    @LifeExecutable
+    public static boolean greaterThan(long arg1, long arg2) {
+        return arg1 == arg2;
+    }
+
+
+}
