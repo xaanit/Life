63dcb664deaf19d46998b486c2bf564b0d0cc9a9
xaanit <shadowjacob@yahoo.com>
2017-09-29 10:06:15 -0500

    Switching to manual parsing

--- src/main/java/me/xaanit/life/internal/convert/MatcherVariables.java
+++ src/main/java/me/xaanit/life/internal/convert/MatcherVariables.java
@@ -1,10 +1,18 @@
 package me.xaanit.life.internal.convert;
 
-public enum MatcherVariables {
-	IDENTIFIER_FIRST("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
-	IDENTIFIER_SECOND(IDENTIFIER_FIRST.info + "_0123456789"),
-	EVERY_CHARACTER(IDENTIFIER_SECOND.info + ";*+=\\{}%&| ()\r\n.\"'-/");
+import java.util.regex.Pattern;
 
+public enum MatcherVariables {
+	IDENTIFIER_FIRST("[a-zA-Z]"),
+	IDENTIFIER_SECOND(IDENTIFIER_FIRST.info + "[]"),
+	METHOD(""),
+	VARIABLE("(const\\s+)?(.+)\\s+(.+)\\s+=\\s+(.+);"),
+	VARIABLE_EDIT("(.+)\\s+=\\s+(.+);"),
+	IF("if\\((.+)\\)"),
+	ELIF("el" + IF.info),
+	ELSE("else"),
+	FOR("for\\((([0-9]+)\\s+to\\s+([0-9]+)\\s+by\\s+(.+))\\)"),
+	WHILE("while\\((.+)\\)");
 
 
 	private final String info;
@@ -15,6 +23,10 @@ public enum MatcherVariables {
 
 	public String getInfo() {
 		return this.info;
+	}
+
+	public final Pattern getPattern() {
+		return Pattern.compile(info);
 	}
 
 	@Override
