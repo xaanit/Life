d05ce0c7c3dcea04057f4e54ea3152cab61e05c0
xaanit <shadowjacob@yahoo.com>
2017-10-02 23:42:46 -0500

    Conditionals are hard

--- src/main/java/me/xaanit/life/internal/matching/ConditionalMatcher.java
+++ src/main/java/me/xaanit/life/internal/matching/ConditionalMatcher.java
@@ -1,5 +1,6 @@
 package me.xaanit.life.internal.matching;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 import me.xaanit.life.internal.exceptions.TokeniserException;
 
@@ -9,7 +10,7 @@ public class ConditionalMatcher {
 
 	public static boolean match(String conditional) {
 		System.out.println("CONDITIONAL: " + conditional);
-		if(integer.get() == 5) {
+		if(integer.get() == 10) {
 			System.exit(0);
 		}
 		integer.incrementAndGet();
@@ -35,7 +36,8 @@ public class ConditionalMatcher {
 			}
 			String temp = conditional.substring(lastIndexOfOpen, firstIndexOfClosed + 1);
 			System.out.println("TEMP: " + temp);
-			conditional = conditional.replace(temp, handle(temp, conditional.charAt(lastIndexOfOpen - 1) == '!') + "");
+			conditional = conditional
+					.replace(temp, handle(temp, conditional.charAt(lastIndexOfOpen - 1) == '!') + "");
 			System.out.println("CONDITIONAL: " + conditional);
 			System.out.println();
 			return match(conditional);
@@ -43,16 +45,55 @@ public class ConditionalMatcher {
 	}
 
 	private static boolean handle(String part, boolean isFalse) {
-		part = Tokenisable.trim(part);
+		part = part.replaceAll("\\s+", "");
 		if(part.equals("true") || part.equals("false")) {
 			return part.equals("true");
 		} else {
+			if(part.contains("&&")) {
+				String[] split = part.split("&&");
+				if(split.length == 1) {
+					throw new TokeniserException("Invalid conditional in if statement!");
+				}
+				String find = "";
+				String before = "";
+				String after = "";
+				System.out.println("SPLIT: " + Arrays.toString(split));
+				for(int i = 0; i < split.length; i++) {
+					if(!split[i].equals("true") && !split[i].equals("false")) {
+						find = split[i];
+						before = Tokenisable.trim(combine(split, 0, i)).replaceAll("&&", "")
+								.replaceAll("\\s+", " && ");
+						after = Tokenisable.trim(combine(split, i + 1, -1)).replaceAll("\\s+", " && ");
+						break;
+					}
+				}
+				if(after.isEmpty() && !before.isEmpty()) {
+					before += "&&";
+				}
+				System.out.println("find: " + find);
+				System.out.println("before: " + before);
+				System.out.println("after: " + after);
+				System.out.printf("Full: %smatch(%s)%s\n",
+						before.isEmpty() ? "" : before + (!after.isEmpty() ? " && " : ""), find,
+						after.isEmpty() ? "" : " && " + after);
+
+				return true;
+			}
 			String temp = "";
 			boolean bool = false;
 			boolean not = false;
-
 			return handle(part.replace(temp, Boolean.toString(bool)), not);
 		}
+	}
+
+	private static String combine(String[] args, int start, int end) {
+		end = end == -1 ? args.length : end;
+		start = start == -1 ? 0 : start;
+		StringBuilder res = new StringBuilder();
+		for(int i = start; i < end; i++) {
+			res.append(args[i]).append(i == end - 1 ? "" : " ");
+		}
+		return res.toString();
 	}
 
 
